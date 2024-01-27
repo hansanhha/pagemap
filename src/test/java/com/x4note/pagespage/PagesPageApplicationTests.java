28249a0653eb19f7babd6a8c0aa6fb41fe2646f3
hansanhha <kwixs35@gmail.com>
2024-01-27 15:35:18 +0900

    first commit

---
+++ src/test/java/com/x4note/pagespage/PagesPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x4note.pagespage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagesPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

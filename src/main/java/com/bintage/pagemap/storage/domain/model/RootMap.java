2bd502122d603a8bf7a7d33b1babe6ac829ad54a
hansanhha <kwixs35@gmail.com>
2024-06-06 11:58:22 +0900

    [storage] RootMap domain model 및 RootMapRepository 추가

---
+++ src/main/java/com/bintage/pagemap/storage/domain/model/RootMap.java
@@ -0,0 +1,51 @@
+package com.bintage.pagemap.storage.domain.model;
+
+import com.bintage.pagemap.auth.domain.account.Account;
+import lombok.Builder;
+import lombok.Getter;
+import org.jmolecules.ddd.annotation.AggregateRoot;
+
+import java.util.Set;
+
+@AggregateRoot
+@Builder
+@Getter
+public class RootMap {
+
+    private final Account.AccountId accountId;
+    private final Map.MapId id;
+    private Set<Map> children;
+    private Set<WebPage> webPages;
+
+    public void addChild(Map child) {
+        if (children.contains(child)) {
+            throw new StorageException.AlreadyItemExistException(StorageException.Item.MAP, child.getId().value().toString());
+        }
+
+        children.add(child);
+    }
+
+    public void removeChild(Map child) {
+        if (!children.contains(child)) {
+            throw new StorageException.NotExistContainItemException(StorageException.Item.MAP, child.getId().value().toString());
+        }
+
+        children.remove(child);
+    }
+
+    public void addPage(WebPage webPage) {
+        if (webPages.contains(webPage)) {
+            throw new StorageException.AlreadyContainItemException(StorageException.Item.PAGE, webPage.getId().value());
+        }
+
+        webPages.add(webPage);
+    }
+
+    public void removePage(WebPage webPage) {
+        if (!webPages.contains(webPage)) {
+            throw new StorageException.NotExistContainItemException(StorageException.Item.PAGE, webPage.getId().value());
+        }
+
+        webPages.remove(webPage);
+    }
+}

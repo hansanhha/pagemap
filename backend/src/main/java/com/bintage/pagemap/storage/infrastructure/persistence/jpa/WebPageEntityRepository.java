d9914840669c50551723ab621e7d27118538c0d4
hansanhha <kwixs35@gmail.com>
2024-07-02 20:25:17 +0900

    [BE] DAO 계층 수정

--- backend/src/main/java/com/bintage/pagemap/storage/infrastructure/persistence/jpa/WebPageEntityRepository.java
+++ backend/src/main/java/com/bintage/pagemap/storage/infrastructure/persistence/jpa/WebPageEntityRepository.java
@@ -4,10 +4,15 @@ import com.bintage.pagemap.storage.infrastructure.persistence.jpa.entity.WebPage
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public interface WebPageEntityRepository extends JpaRepository<WebPageEntity, Long> {
 
-    @Query("SELECT w FROM WebPageEntity w WHERE w.parentMap = :parentMap")
-    List<WebPageEntity> findAllByParentMap(Long parentMap);
+    @Query("SELECT w FROM WebPageEntity w LEFT JOIN FETCH w.categories LEFT JOIN FETCH w.tags WHERE w.accountId = :accountId AND w.parentMap = :parentMap")
+    List<WebPageEntity> findAllByParentMap(String accountId, Long parentMap);
+
+    @Query("SELECT w FROM WebPageEntity w LEFT JOIN FETCH w.categories wc LEFT JOIN FETCH w.tags WHERE w.accountId = :accountId AND wc IN :categoryId")
+    List<WebPageEntity> findAllByAccountIdAndCategoryId(String accountId, Long categoryId);
 }

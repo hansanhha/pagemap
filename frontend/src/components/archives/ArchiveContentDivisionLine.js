882387c76f2f72fbb3a0457d4ca1f17c349bffa5
hansanhha <kwixs35@gmail.com>
2024-07-17 11:06:06 +0900

    refactor(ui): 기존 components, pages 삭제

--- frontend/src/components/archives/ArchiveContentDivisionLine.js
+++
@@ -1,9 +0,0 @@
-import Style from "./styles/ArchiveDetails.module.css";
-
-function ArchiveContentDivisionLine() {
-    return (
-        <div className={Style.archive_content_division_line}></div>
-    )
-}
-
-export default ArchiveContentDivisionLine;

3133780af5c55baa7bbb1e861fef305d3f050ab6
hansanhha <kwixs35@gmail.com>
2024-07-25 00:30:31 +0900

    refactor(ui): dto 수정

--- frontend/src/service/dto/ShortcutDto.js
+++ frontend/src/service/dto/ShortcutDto.js
@@ -1,15 +1,15 @@
 class ShortcutDto {
     id;
     logo;
-    title;
-    url;
+    name;
+    uri;
     order;
 
     constructor(Shortcut) {
         this.id = Shortcut.id;
         this.logo = null;
-        this.title = Shortcut.title;
-        this.url = Shortcut.url;
+        this.name = Shortcut.name;
+        this.uri = Shortcut.uri;
         this.order = 0;
     }
 
